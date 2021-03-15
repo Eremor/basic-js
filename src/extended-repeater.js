77f515ae4efa1300008ed80b06471600229438bc
Eremor <artem.s13@gmail.com>
2021-03-15 14:00:27 +0300

    feat: solution task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let arr = [];
+  let result = '';
+  let addAddition = '';
+  let repeatTimes = options.repeatTimes || 1;
+  let separator = options.separator || '+';
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let addition = '';
+  if(options.hasOwnProperty('addition')) addition = options.addition + '';
+  let additionSeparator = options.additionSeparator ? options.additionSeparator + '' : '|';
+  str = str + '';
+
+  for(let i = 0 ; i < repeatTimes; i++) {
+    arr.push(str)
+  }
+
+  for(let j = 0; j < additionRepeatTimes; j++) {
+    for(let n = 0; n < arr.length; n++) {
+      if(j < additionRepeatTimes - 1) {
+        addAddition = `${addition}${additionSeparator}`
+      } else {
+        addAddition = addition;
+      }
+      arr[n] += addAddition
+    }
+  }
+  
+  if(repeatTimes  == 1) {
+    result = arr.join('')
+  } else {
+    result = arr.join(separator)
+  }
+
+  return result
 };
   
